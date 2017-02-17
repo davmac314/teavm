ca8fcb9065d309ae906b1e89b437b6dd6c634680
Davin McCall <davmac@davmac.org>
2017-02-17 10:17:33 +0000

    Modify BuildTarget interface to be able to append to an existing resource.
    This is necessary for TeaVMTool to be able to correctly finalise
    Javascript output.

--- tests/src/test/java/org/teavm/dependency/ClassValueTest.java
+++ tests/src/test/java/org/teavm/dependency/ClassValueTest.java
@@ -17,6 +17,10 @@ package org.teavm.dependency;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
 import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.junit.Test;
 import org.teavm.backend.javascript.JavaScriptTarget;
@@ -24,6 +28,7 @@ import org.teavm.model.MethodReference;
 import org.teavm.model.ValueType;
 import org.teavm.tooling.TeaVMProblemRenderer;
 import org.teavm.tooling.TeaVMToolLog;
+import org.teavm.vm.BuildTarget;
 import org.teavm.vm.TeaVM;
 import org.teavm.vm.TeaVMBuilder;
 
@@ -78,7 +83,22 @@ public class ClassValueTest {
         TeaVM vm = new TeaVMBuilder(new JavaScriptTarget()).build();
         vm.installPlugins();
         vm.entryPoint(new MethodReference(getClass().getName(), methodName, ValueType.VOID));
-        vm.build(fileName -> new ByteArrayOutputStream(), "tmp");
+        //vm.build(fileName -> new ByteArrayOutputStream(), "tmp");
+        vm.build(new BuildTarget() {
+            @Override
+            public OutputStream createResource(String fileName)
+                    throws IOException
+            {
+                return new ByteArrayOutputStream();
+            }
+
+            @Override
+            public OutputStream appendResource(String fileName)
+                    throws IOException
+            {
+                return new ByteArrayOutputStream();
+            }
+        }, "tmp");
         if (!vm.getProblemProvider().getSevereProblems().isEmpty()) {
             fail("Code compiled with errors:\n" + describeProblems(vm));
         }
--- tools/core/src/main/java/org/teavm/tooling/TeaVMTool.java
+++ tools/core/src/main/java/org/teavm/tooling/TeaVMTool.java
@@ -454,7 +454,8 @@ public class TeaVMTool implements BaseTeaVMTool {
             }
 
             if (targetType == TeaVMTargetType.JAVASCRIPT) {
-                try (OutputStream output = new FileOutputStream(new File(targetDirectory, outputName), true)) {
+                String outputPath = new File(targetDirectory, outputName).getPath();
+                try (OutputStream output = buildTarget.appendResource(outputPath)) {
                     try (Writer writer = new OutputStreamWriter(output, "UTF-8")) {
                         additionalJavaScriptOutput(writer);
                     }
